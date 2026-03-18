bbb65a662ec39b93b0e5703d79ce06b94d9487e0
两个人走 <969141758@qq.com>
2026-03-19 01:10:03 +0800

    fix: add h-full to MainContent container for proper height inheritance

--- src/App.tsx
+++ src/App.tsx
@@ -476,7 +476,7 @@ function MainContent() {
   };
 
   return (
-    <div className="flex-1 overflow-hidden">
+    <div className="flex-1 overflow-hidden h-full">
       {renderContent()}
     </div>
   );
